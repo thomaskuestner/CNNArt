66d177c021300a2fa9a3d11827a1810ba26aa6bf
TokenJan <jiahuan.yang@outlook.com>
2018-03-02 14:26:01 +0100

    fix unpatching

--- correction/networks/motion/VAE2D/motion_head_VAE2D_80.py
+++ correction/networks/motion/VAE2D/motion_head_VAE2D_80.py
@@ -126,11 +126,11 @@ def fTrainInner(dData, sOutPath, patchSize, epochs, batchSize, lr, kl_weight, pi
 
     print('Training with epochs {} batch size {} learning rate {}'.format(epochs, batchSize, lr))
 
-    weights_file = sOutPath + os.sep + 'vae_weight_ps_{}_bs_{}_lr_{}_l2.h5'.format(patchSize[0], batchSize, lr)
+    weights_file = sOutPath + os.sep + 'vae_weight_ps_{}_bs_{}_lr_{}.h5'.format(patchSize[0], batchSize, lr)
 
-    callback_list = [EarlyStopping(monitor='val_loss', patience=5, verbose=1)]
+    callback_list = [EarlyStopping(monitor='val_loss', patience=10, verbose=1)]
     callback_list.append(ModelCheckpoint(weights_file, monitor='val_loss', verbose=1, period=1, save_best_only=True, save_weights_only=True))
-    callback_list.append(ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=3, min_lr=1e-5, verbose=1))
+    callback_list.append(ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5, min_lr=1e-5, verbose=1))
 
     history = vae.fit([train_ref, train_art],
             shuffle=True,
@@ -195,6 +195,6 @@ def fPredict(dData, sOutPath, patchSize, dHyper, lSave, unpatch):
                 axes[j, 1].imshow(predict_art[4*i+j])
 
             if lSave:
-                plt.imsave(sOutPath + os.sep + 'result' + os.sep + str(i) + '.png')
+                plt.savefig(sOutPath + os.sep + 'result' + os.sep + str(i) + '.png')
             else:
                 plt.show()
