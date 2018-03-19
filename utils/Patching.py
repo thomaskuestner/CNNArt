6e44553e73706ed22d598131ed6b091178e03ac9
Thomas Kuestner <thomas.kuestner@iss.uni-stuttgart.de>
2018-03-19 17:21:46 +0000

    correct indexing

--- utils/Patching.py
+++ utils/Patching.py
@@ -46,7 +46,7 @@ def fRigidPatching(dicom_numpy_array, patchSize, patchOverlap, mask_numpy_array,
                     dPatches[:,:,idxPatch] = dPatch
                     idxPatch += 1
 
-        dPatches = dPatches[:, :, 0:idxPatch-1]
+        dPatches = dPatches[:, :, 0:idxPatch]
         dLabels = np.ones((dPatches.shape[2]))
     elif sLabeling == 'patch':
         Mask_zero_pad = np.lib.pad(mask_numpy_array, (
@@ -93,8 +93,8 @@ def fRigidPatching(dicom_numpy_array, patchSize, patchOverlap, mask_numpy_array,
                     shim_artefact = False
                     noise_artefact = False
 
-        dPatches = dPatches[:, :, 0:idxPatch-1]
-        dLabels = dLabels[0:idxPatch-1]
+        dPatches = dPatches[:, :, 0:idxPatch]
+        dLabels = dLabels[0:idxPatch]
     return dPatches, dLabels
 ##########################################################################################################################################
 # In case of 3D patches:                                                                                                                 #
@@ -137,7 +137,7 @@ def fRigidPatching3D(dicom_numpy_array, patchSize, patchOverlap, mask_numpy_arra
                     dPatches[:,:,:,idxPatch] = dPatch
                     idxPatch += 1
 
-        dPatches = dPatches[:, :, :, 0:idxPatch-1]
+        dPatches = dPatches[:, :, :, 0:idxPatch]
         dLabels = np.ones((dPatches.shape[3]))
     elif sLabeling == 'patch':
         Mask_zero_pad = np.lib.pad(mask_numpy_array, (
@@ -183,6 +183,6 @@ def fRigidPatching3D(dicom_numpy_array, patchSize, patchOverlap, mask_numpy_arra
                     shim_artefact = False
                     noise_artefact = False
                     
-        dPatches = dPatches[:, :, :, 0:idxPatch-1]
-        dLabels = dLabels[0:idxPatch-1]
+        dPatches = dPatches[:, :, :, 0:idxPatch]
+        dLabels = dLabels[0:idxPatch]
     return dPatches, dLabels
