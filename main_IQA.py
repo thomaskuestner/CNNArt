551d606172f1a196c9e176541bc4f72bdda1e473
yangsi <yangshanqi_m@outlook.com>
2019-01-14 16:17:55 +0100

    add rough scripts about IQA

---
+++ main_IQA.py
@@ -0,0 +1,119 @@
+import os
+import tensorflow as tf
+import glob
+import sys
+
+sys.path.append('/home/d1274/PycharmProjects/NAKO_transfer_learning')
+from tensorflow.keras import backend as K
+from tensorflow.python import keras
+
+from model import multiclass_3D_SE_ResNet
+from util import get_train_eval_files
+from util import get_train_eval_files_NAKO_IQA
+from util import generator
+from util import patches
+
+config = tf.ConfigProto()
+os.environ["CUDA_VISIBLE_DEVICES"] = '1'
+# config.gpu_options.per_process_gpu_memory_fraction = 0.5
+config.gpu_options.allow_growth = True
+tf.keras.backend.set_session(tf.Session(config=config))
+
+import tensorflow.keras.optimizers as optimizers
+
+from tensorflow.keras.callbacks import (
+    CSVLogger,
+    ModelCheckpoint
+)
+
+if __name__ == '__main__':
+    print('begin creating the input dataset')
+
+
+    epoch = 200
+    path = '/home/d1274/med_data/NAKO/NAKO_IQA_tf'
+
+
+    batch_size = 48
+    image_shape = [236, 320, 260]
+    patch_shape = [64, 64, 64]
+    start = [20, 20, 20]
+    num_images_loaded = 30
+    overlap = 32
+
+    # there are two ways to extract the image, start from the beginning bprder or start from the ned border
+    patches_per_image_1 = len(patches.compute_patch_indices(image_shape=image_shape,
+                                      patch_size=patch_shape,
+                                      overlap=overlap,
+                                      start=start,
+                                      order = True))
+    patches_per_image_2 = len(patches.compute_patch_indices(image_shape=image_shape,
+                                                            patch_size=patch_shape,
+                                                            overlap=overlap,
+                                                            start=start,
+                                                            order = False))
+    assert patches_per_image_1 == patches_per_image_2
+    patches_per_image = patches_per_image_1
+
+    train_files, train_labels, eva_files, eva_labels = get_train_eval_files_NAKO_IQA.get_train_eval_files(path, pattern='_F_')
+
+    steps_per_epoch = int(len(train_files) * patches_per_image / batch_size)
+    validata_steps = int(len(eva_files) * patches_per_image / batch_size)
+    print('expected step per epoch: ' , steps_per_epoch)
+
+    # construct the model
+    model, _ = multiclass_3D_SE_ResNet.createModel(patchSize=patch_shape, numClasses=3)
+    print(model.summary())
+
+    # optimize way ???
+    learning_rate = 0.1
+    decay_rate = learning_rate / epoch
+    momentum = 0.8
+    sgd = optimizers.SGD(lr=learning_rate, momentum=momentum, decay=decay_rate, nesterov=False)
+    model.compile(loss="categorical_crossentropy",
+                  optimizer="sgd",
+                  metrics=['acc'])
+
+
+    def get_callbacks(model_file, logging_file=None, early_stopping_patience=None,
+                      initial_learning_rate=0.01, lr_change_mode=None, verbosity=1):
+        callbacks = list()
+
+        # save the model
+        callbacks.append(ModelCheckpoint(model_file, monitor='val_loss', save_best_only=True, mode='auto'))
+
+        # records the basic metrics
+        callbacks.append(CSVLogger(logging_file, append=True))
+        return callbacks
+
+
+    model_file = os.path.join('/home/d1274/no_backup/d1274/NAKO_transfer_learning_model/NAKO_IQA_model',
+                              'simple_test_1.h5')
+    logging_file = 'simple_test_1.log'
+
+    train_generator = generator.tfdata_generator(file_lists=train_files, label_lists=train_labels,
+                                                 is_training=True,
+                                                 image_shape= image_shape,
+                                                 patch_size=patch_shape,
+                                                 start=start,
+                                                 batch_size=batch_size,
+                                                 overlap = overlap,
+                                                 num_imgaes_loaded=num_images_loaded)
+
+    val_generator = generator.tfdata_generator(file_lists=eva_files, label_lists=eva_labels,
+                                               is_training=False,
+                                               image_shape= image_shape,
+                                               patch_size=patch_shape,
+                                               start= start,
+                                               overlap = overlap,
+                                               batch_size=batch_size)
+
+    res = model.fit(
+        train_generator.make_one_shot_iterator(),
+        steps_per_epoch=steps_per_epoch,
+        epochs=epoch,
+        validation_data=val_generator.make_one_shot_iterator(),
+        validation_steps=validata_steps,
+        callbacks=get_callbacks(model_file=model_file, logging_file=logging_file))
+
+    print('the result: ', res)
---
+++ utils/generator.py
@@ -0,0 +1,90 @@
+import tensorflow as tf
+from utils import Patching
+from utils import tfrecord
+
+from tensorflow.keras import backend as K
+
+
+# [320, 260, 316] is the shape for NAKO dataset
+# [236, 320, 260] is the shape for NAKO_IQA dataset
+
+def tfdata_generator(file_lists, label_lists, is_training, num_parallel_calls=4, patch_size=[64, 64, 64],
+                     overlap=32, image_shape=[316, 260, 320], start=[20, 60, 50],
+                     num_imgaes_loaded=30, batch_size=32, prefetched_buffer_size=8000, ):
+    # begin construct tensorflow input pipeline
+    NUM_CLASSES = 3
+    # define the patch you want to crop
+    len_file_lists = len(file_lists)
+    patch_size = patch_size  # size of the patches per axis
+    overlap = overlap
+    image_shape = image_shape
+    start = start
+
+    # number of patches to extract from each image
+    num_patches = len(Patching.compute_patch_indices(image_shape=image_shape,
+                                                    patch_size=patch_size,
+                                                    overlap=overlap,
+                                                    start=start))
+
+    buffer_size = num_imgaes_loaded * num_patches  # shuffle patches from 34 different big images
+
+    # This function splits a whole image into many splits and return
+    get_patches_fn = lambda image: Patching.get_patches(image,
+                                                       num_patches=num_patches,
+                                                       image_shape=image_shape,
+                                                       patch_size=patch_size,
+                                                       overlap=overlap,
+                                                       start=start)
+
+    filenames = tf.data.TFRecordDataset(file_lists)
+    labels = tf.data.Dataset.from_tensor_slices(label_lists)
+    dataset = tf.data.Dataset.zip((filenames, labels))
+    dataset = dataset.shuffle(buffer_size=len_file_lists)
+
+    # Note: this could be rewritten as one map call
+    # map parse function to each zipped element
+    dataset = dataset.map(
+        map_func=lambda a, b: (tfrecord.medio.convert_tf.parse_function_image(a), b),
+        num_parallel_calls=num_parallel_calls)
+
+    dataset = dataset.map(
+        map_func=lambda a, b: (tf.cast(a, tf.float32), b),
+        num_parallel_calls=num_parallel_calls)
+
+    # normalization
+    mean = tf.constant(78.242, dtype=tf.float32)
+    std = tf.constant(144.83, dtype=tf.float32)
+
+    dataset = dataset.map(
+        map_func=lambda a, b: (tf.subtract(a, mean), b),
+        num_parallel_calls=num_parallel_calls)
+    dataset = dataset.map(
+        map_func=lambda a, b: (tf.divide(a, std), b),
+        num_parallel_calls=num_parallel_calls)
+
+    dataset = dataset.map(map_func=lambda a, b: (get_patches_fn(a),
+                                                 [b for _ in range(num_patches)]),
+                          num_parallel_calls=num_parallel_calls)
+
+    dataset = dataset.apply(tf.contrib.data.unbatch())
+
+    dataset = dataset.map(
+        map_func=lambda a, b: (tf.reshape(a, patch_size), b),
+        num_parallel_calls=num_parallel_calls)
+
+    dataset = dataset.map(
+        map_func=lambda a, b: (tf.expand_dims(a, -1),
+                               tf.one_hot(b, NUM_CLASSES),),
+        num_parallel_calls=num_parallel_calls)
+
+    # here one could use shuffle, repeat, prefetch, ...
+    if is_training:
+        dataset = dataset.shuffle(buffer_size=buffer_size)
+
+    dataset_batched = dataset.batch(batch_size=batch_size)
+    # dataset_batched = dataset
+    # ???
+    # dataset_batched = dataset_batched.prefetch(buffer_size = prefetched_buffer_size)
+    dataset_batched = dataset_batched.repeat()
+
+    return dataset_batched
