0379cc450d5d87f3cd03596f4f05481cf8557a89
Yi Zhang <43481370+yiyi1zhang@users.noreply.github.com>
2019-03-25 12:14:08 +0100

    Add files via upload

---
+++ config/PATH.py
@@ -0,0 +1,25 @@
+import yaml
+with open('config/param.yml', 'r') as ymlfile:
+    cfg = yaml.safe_load(ymlfile)
+
+PATH_OUT = cfg['pathout']
+
+PREDICT_IMG_PATH = cfg['lPredictImg']
+
+DLART_OUT_PATH = cfg['DLart_output_path']
+
+GPU_HOME_PATH = cfg['gpu_home']
+
+PREDICT_RESULT_PATH = cfg['predict_result']
+
+TEST_PATH = cfg['test_path']
+
+CNN_PATH = cfg['cnn_path']
+
+MARKING_PATH = cfg['marking_path']
+
+LABEL_PATH = cfg['label_path']
+
+LEARNING_OUT = cfg['output_learning']
+
+CHECK_PATH = cfg['check_point']
