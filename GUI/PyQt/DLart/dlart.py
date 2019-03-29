677b2809f2ffecbec962c14667c9ea92cfa911be
Thomas Kuestner <thomas.kuestner@kcl.ac.uk>
2019-03-29 17:33:13 +0000

    rebase dlart gui

--- GUI/PyQt/DLart/dlart.py
+++ GUI/PyQt/DLart/dlart.py
@@ -1,5 +1,8 @@
-import datetime
-from collections import Counter
+'''
+@author: Yannick Wilhelm
+@email: yannick.wilhelm@gmx.de
+@date: January 2018
+'''
 
 from DLart.DataPreprocessing import create_MASK_Array
 from DLart.Dataset import Dataset
@@ -156,6 +159,28 @@ class DeepLearningArtApp():
         self.X_test = None
         self.Y_test = None
         self.Y_segMasks_test = None
+
+
+        ####################
+        ### ArtGAN Stuff ###
+        ####################
+        self.patients_ArtGAN = None
+        self.datasets_ArtGAN = None
+        self.datasets_ArtGAN_Pairs = None
+
+        self.patchSizeX_ArtGAN = 40
+        self.patchSizeY_ArtGAN = 40
+        self.patchSizeZ_ArtGAN = 5
+        self.patchOverlap_ArtGAN = 0.5
+
+        self.lscaleFactor_ArtGAN = [0.5, 1, 2]
+
+        self.storeMode_ArtGAN = DeepLearningArtApp.STORE_DISABLED
+        self.splittingMode_ArtGAN = DeepLearningArtApp.SIMPLE_RANDOM_SAMPLE_SPLITTING
+
+        self.trainTestDatasetRatio_ArtGAN = 0.2  # part of test data
+        self.trainValidationRatio_ArtGAN = 0.0  # part of Validation data in traindata
+        ####################
 
         ################################################################################################################
         #### Stuff for prediction
