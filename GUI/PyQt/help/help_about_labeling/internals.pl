911eeed2e32340c881ce2d9b1aff639b71f58c28
Yi Zhang <43481370+yiyi1zhang@users.noreply.github.com>
2019-04-07 11:04:42 +0200

    Add files via upload

---
+++ GUI/PyQt/help/help_about_labeling/internals.pl
@@ -0,0 +1,34 @@
+# LaTeX2HTML 2018 (Released Feb 1, 2018)
+# Associate internals original text with physical files.
+
+
+$key = q/fig:create_label/;
+$ref_files{$key} = "$dir".q|node1.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:cursor_inspector/;
+$ref_files{$key} = "$dir".q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:edit_default_label/;
+$ref_files{$key} = "$dir".q|node1.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:edit_label/;
+$ref_files{$key} = "$dir".q|node1.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:select_ROI/;
+$ref_files{$key} = "$dir".q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:set_result_color/;
+$ref_files{$key} = "$dir".q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:show_result/;
+$ref_files{$key} = "$dir".q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
