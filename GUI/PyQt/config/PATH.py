8f6acd705b3dc5af130c8564db631f9afe2027e4
Yi Zhang <43481370+yiyi1zhang@users.noreply.github.com>
2019-04-28 21:18:03 +0200

    Add files via upload
    Removing useless paths

--- GUI/PyQt/config/PATH.py
+++ GUI/PyQt/config/PATH.py
@@ -11,22 +11,10 @@ SUBDIRS = cfg['subdirs']
 
 PATH_OUT = cfg['pathout']
 
-PREDICT_IMG_PATH = cfg['lPredictImg']
-
-DLART_OUT_PATH = cfg['DLart_output_path']
-
 GPU_HOME_PATH = cfg['gpu_home']
-
-PREDICT_RESULT_PATH = cfg['predict_result']
-
-TEST_PATH = cfg['test_path']
-
-CNN_PATH = cfg['cnn_path']
-
-MARKING_PATH = cfg['marking_path']
 
 LABEL_PATH = cfg['label_path']
 
 LEARNING_OUT = cfg['output_learning']
 
-CHECK_PATH = cfg['check_point']
+GRAPHVIZ_PATH = cfg['graphviz_path']
