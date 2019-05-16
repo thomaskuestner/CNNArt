734f9abc2a8881c7701e4ab796f6e6bf63fd4041
HonghaoGu <ghh416866603@gmail.com>
2019-05-16 14:59:55 +0200

    path

--- utils/tfrecord/training/train.py
+++ utils/tfrecord/training/train.py
@@ -5,13 +5,13 @@ import sys
 from keras import backend as K
 from tensorflow.python import keras
 #from _3D_ResNet import Resnet3DBuilder
-from multiclass_3D_CNN import buildModel
-from create_dataset import create_dataset
+from utils.tfrecord.training.multiclass_3D_CNN import buildModel
+from utils.tfrecord.training.create_dataset import create_dataset
 #from create_dataset_copy import create_dataset
 
 config = tf.ConfigProto()
 os.environ["CUDA_VISIBLE_DEVICES"]= "0"
-#config.gpu_options.per_process_gpu_memory_fraction = 0.5
+config.gpu_options.per_process_gpu_memory_fraction = 0.5
 config.gpu_options.allow_growth = True
 tf.keras.backend.set_session(tf.Session(config = config))
 
@@ -19,7 +19,7 @@ tf.keras.backend.set_session(tf.Session(config = config))
 if __name__ == '__main__':
 
     print('begin creating the input dataset')
-    data_dir = '/home/d1274/no_backup/d1274/data'
+    data_dir = '/home/s1304/med_data/NAKO/NAKO_IQA/'
 
     datagen= create_dataset(data_dir)
 
@@ -33,7 +33,7 @@ if __name__ == '__main__':
                   optimizer="sgd",
                   metrics=['acc'])
 
-    model_dir = os.path.join('/home/d1274/no_backup/d1274/model', "IQA_test_with_tfrecord")
+    model_dir = os.path.join('/home/s1304/med_data/NAKO/NAKO_IQA/', "IQA_test_with_tfrecord")
     os.makedirs(model_dir, exist_ok=True)
     print("model_dir: ",model_dir)
     est_iqa = tf.keras.estimator.model_to_estimator(keras_model=model,
