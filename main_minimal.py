ad3a15f5ca61cddc7884daff1bb5ea0c9892590b
Thomas Kuestner <thomas.kuestner@kcl.ac.uk>
2019-06-07 15:31:14 +0100

    add minimal working main

--- main_minimal.py
+++ main_minimal.py
@@ -1,3 +1,7 @@
+'''
+Copyright: 2016-2019 Thomas Kuestner (thomas.kuestner@med.uni-tuebingen.de) under Apache2 license
+@author: Thomas Kuestner
+'''
 # imports
 import sys
 import numpy as np                  # for algebraic operations, matrices
@@ -6,7 +10,11 @@ import scipy.io as sio              # I/O
 import os                      # operating system
 import argparse
 from utils.data import *
+from utils.dlnetwork import *
 import utils.Label as Label
+import datetime
+import yaml
+
 
 def fParseConfig(sFile):
     # get config file
@@ -16,7 +24,7 @@ def fParseConfig(sFile):
     return cfg
 
 
-def performTraining(data):
+def fArtDetection(data, dlnetwork, sMode):
     # set GPU
     os.environ["CUDA_VISIBLE_DEVICES"] = str(data.gpu_id)
 
@@ -26,74 +34,72 @@ def performTraining(data):
                                                         usingBodyRegion=data.usingBodyRegions,
                                                         usingTWeightings=data.usingTWeighting)
 
+    Y_train = []
 
+    Y_validation = []
 
-        Y_train = []
+    Y_test = []
 
-        Y_validation = []
+    data.Y_segMasks_train[data.Y_segMasks_train == 3] = 1
+    data.Y_segMasks_train[data.Y_segMasks_train == 2] = 0
 
-        Y_test = []
+    data.Y_segMasks_test[data.Y_segMasks_test == 3] = 1
+    data.Y_segMasks_test[data.Y_segMasks_test == 2] = 0
 
-        data.Y_segMasks_train[data.Y_segMasks_train == 3] = 1
-        data.Y_segMasks_train[data.Y_segMasks_train == 2] = 0
+    ##########################
+    ###########################
+    # for generating patch labels
 
-        data.Y_segMasks_test[data.Y_segMasks_test == 3] = 1
-        data.Y_segMasks_test[data.Y_segMasks_test == 2] = 0
+    y_labels_train = np.expand_dims(data.Y_segMasks_train, axis=-1)
+    y_labels_train[y_labels_train == 0] = -1
+    y_labels_train[y_labels_train == 1] = 1
+    y_labels_train = np.sum(y_labels_train, axis=1)
+    y_labels_train = np.sum(y_labels_train, axis=1)
+    y_labels_train = np.sum(y_labels_train, axis=1)
+    y_labels_train[y_labels_train >= 0] = 1
+    y_labels_train[y_labels_train < 0] = 0
+    for i in range(y_labels_train.shape[0]):
+        Y_train.append([1, 0] if y_labels_train[i].all() == 0 else [0, 1])
+    Y_train = np.asarray(Y_train)
 
-        ##########################
-        ###########################
-        # for generating patch labels
+    y_labels_test = np.expand_dims(data.Y_segMasks_test, axis=-1)
+    y_labels_test[y_labels_test == 0] = -1
+    y_labels_test[y_labels_test == 1] = 1
+    y_labels_test = np.sum(y_labels_test, axis=1)
+    y_labels_test = np.sum(y_labels_test, axis=1)
+    y_labels_test = np.sum(y_labels_test, axis=1)
+    y_labels_test[y_labels_test >= 0] = 1
+    y_labels_test[y_labels_test < 0] = 0
 
-        y_labels_train = np.expand_dims(data.Y_segMasks_train, axis=-1)
-        y_labels_train[y_labels_train == 0] = -1
-        y_labels_train[y_labels_train == 1] = 1
-        y_labels_train = np.sum(y_labels_train, axis=1)
-        y_labels_train = np.sum(y_labels_train, axis=1)
-        y_labels_train = np.sum(y_labels_train, axis=1)
-        y_labels_train[y_labels_train >= 0] = 1
-        y_labels_train[y_labels_train < 0] = 0
-        for i in range(y_labels_train.shape[0]):
-            Y_train.append([1, 0] if y_labels_train[i].all() == 0 else [0, 1])
-        Y_train = np.asarray(Y_train)
+    for i in range(y_labels_test.shape[0]):
+        Y_test.append([1, 0] if y_labels_test[i].all() == 0 else [0, 1])
+    Y_test = np.asarray(Y_test)
 
-        y_labels_test = np.expand_dims(data.Y_segMasks_test, axis=-1)
-        y_labels_test[y_labels_test == 0] = -1
-        y_labels_test[y_labels_test == 1] = 1
-        y_labels_test = np.sum(y_labels_test, axis=1)
-        y_labels_test = np.sum(y_labels_test, axis=1)
-        y_labels_test = np.sum(y_labels_test, axis=1)
-        y_labels_test[y_labels_test >= 0] = 1
-        y_labels_test[y_labels_test < 0] = 0
+    # change the shape of the dataset -> at color channel -> here one for grey scale
 
-        for i in range(y_labels_test.shape[0]):
-            Y_test.append([1, 0] if y_labels_test[i].all() == 0 else [0, 1])
-        Y_test = np.asarray(Y_test)
+    # Y_segMasks_train_foreground = np.expand_dims(data.Y_segMasks_train, axis=-1)
+    # Y_segMasks_train_background = np.ones(Y_segMasks_train_foreground.shape) - Y_segMasks_train_foreground
+    # data.Y_segMasks_train = np.concatenate((Y_segMasks_train_background, Y_segMasks_train_foreground),
+    #                                        axis=-1)
+    # data.Y_segMasks_train = np.sum(data.Y_segMasks_train, axis=-1)
+    #
+    # Y_segMasks_test_foreground = np.expand_dims(data.Y_segMasks_test, axis=-1)
+    # Y_segMasks_test_background = np.ones(Y_segMasks_test_foreground.shape) - Y_segMasks_test_foreground
+    # data.Y_segMasks_test = np.concatenate((Y_segMasks_test_background, Y_segMasks_test_foreground),
+    #                                        axis=-1)
+    # data.Y_segMasks_test = np.sum(data.Y_segMasks_test, axis=-1)
 
-        # change the shape of the dataset -> at color channel -> here one for grey scale
-
-        # Y_segMasks_train_foreground = np.expand_dims(data.Y_segMasks_train, axis=-1)
-        # Y_segMasks_train_background = np.ones(Y_segMasks_train_foreground.shape) - Y_segMasks_train_foreground
-        # data.Y_segMasks_train = np.concatenate((Y_segMasks_train_background, Y_segMasks_train_foreground),
-        #                                        axis=-1)
-        # data.Y_segMasks_train = np.sum(data.Y_segMasks_train, axis=-1)
-        #
-        # Y_segMasks_test_foreground = np.expand_dims(data.Y_segMasks_test, axis=-1)
-        # Y_segMasks_test_background = np.ones(Y_segMasks_test_foreground.shape) - Y_segMasks_test_foreground
-        # data.Y_segMasks_test = np.concatenate((Y_segMasks_test_background, Y_segMasks_test_foreground),
-        #                                        axis=-1)
-        # data.Y_segMasks_test = np.sum(data.Y_segMasks_test, axis=-1)
-
-        if data.X_validation.size == 0 and data.Y_validation.size == 0:
-            data.X_validation = 0
-            data.Y_segMasks_validation = 0
-            data.Y_validation = 0
-            print("No Validation Dataset.")
-        else:
-            for i in range(data.Y_validation.shape[0]):
-                Y_validation.append(data.classMappings[data.Y_validation[i]])
-            Y_validation = np.asarray(Y_validation)
-            data.Y_segMasks_validation[data.Y_segMasks_validation == 3] = 1
-            data.Y_segMasks_validation[data.Y_segMasks_validation == 2] = 0
+    if data.X_validation.size == 0 and data.Y_validation.size == 0:
+        data.X_validation = 0
+        data.Y_segMasks_validation = 0
+        data.Y_validation = 0
+        print("No Validation Dataset.")
+    else:
+        for i in range(data.Y_validation.shape[0]):
+            Y_validation.append(data.classMappings[data.Y_validation[i]])
+        Y_validation = np.asarray(Y_validation)
+        data.Y_segMasks_validation[data.Y_segMasks_validation == 3] = 1
+        data.Y_segMasks_validation[data.Y_segMasks_validation == 2] = 0
 
 
 
@@ -120,10 +126,10 @@ def performTraining(data):
     ###################################################################################################################
 
     # output folder
-    data.outPutFolderDataPath = data.learningOutputPath + os.sep + data.neuralNetworkModel + "_"
-    if data.patchingMode == PATCHING_2D:
+    data.outPutFolderDataPath = data.pathOutput + os.sep + dlnetwork.neuralNetworkModel + "_"
+    if data.patchingMode == 'PATCHING_2D':
         data.outPutFolderDataPath += "2D" + "_" + str(data.patchSizeX) + "x" + str(data.patchSizeY)
-    elif data.patchingMode == PATCHING_3D:
+    elif data.patchingMode == 'PATCHING_3D':
         data.outPutFolderDataPath += "3D" + "_" + str(data.patchSizeX) + "x" + str(data.patchSizeY) + \
                                      "x" + str(data.patchSizeZ)
 
@@ -136,30 +142,63 @@ def performTraining(data):
         os.makedirs(data.outPutFolderDataPath + os.sep + 'checkpoints')
 
     # summarize cnn and training
-    data.create_cnn_training_summary(data.neuralNetworkModel, data.outPutFolderDataPath)
+    data.create_cnn_training_summary(dlnetwork.neuralNetworkModel, data.outPutFolderDataPath)
 
     # segmentation FCN training
-    data.model, data.model_name = fRunCNN(dData={'X_train': data.X_train,
-                                                 'y_train': Y_train,
-                                                 'Y_segMasks_train': data.Y_segMasks_train,
-                                                 'X_valid': data.X_validation,
-                                                 'y_valid': Y_validation,
-                                                 'Y_segMasks_validation': data.Y_segMasks_validation,
-                                                 'X_test': data.X_test,
-                                                 'y_test': Y_test,
-                                                 'Y_segMasks_test': data.Y_segMasks_test,
-                                                 'patchSize': [data.patchSizeX, data.patchSizeY,
-                                                               data.patchSizeZ]},
-                                                  sModelIn=data.deepNeuralNetworks[data.neuralNetworkModel],
-                                                  lTrain=RUN_CNN_TRAIN_TEST_VALIDATION,
-                                                  sParaOptim='',
-                                                  sOutPath=data.outPutFolderDataPath,
-                                                  iBatchSize=data.batchSizes,
-                                                  iLearningRate=data.learningRates,
-                                                  iEpochs=data.epochs,
-                                                  dlart_handle=self,
-                                                  usingSegmentationMasks=data.usingSegmentationMasks)
+    # dynamic loading of corresponding model
+    # TODO: case switch here for the different networks
+    sModel = 'networks.FullyConvolutionalNetworks.motion.VResFCN_3D_Upsampling_final_Motion_Binary'
+    cnnModel = __import__(sModel, globals(), locals(), ['fTrain', 'fPredict'],
+                          0)  # dynamic module loading with specified functions and with absolute importing (level=0) -> work in both Python2 and Python3
 
+    if sMode == 'training':
+        if not data.usingSegmentationMasks:
+            cnnModel.fTrain(X_train=data.X_train,
+                            y_train=Y_train,
+                            X_valid=data.X_validation,
+                            y_valid=Y_validation,
+                            X_test=data.X_test,
+                            y_test=Y_test,
+                            sOutPath=data.outPutFolderDataPath,
+                            patchSize=[data.patchSizeX, data.patchSizeY, data.patchSizeZ],
+                            batchSizes=dlnetwork.batchSize,
+                            learningRates=dlnetwork.learningRate,
+                            iEpochs=dlnetwork.epochs,
+                            dlnetwork=dlnetwork)
+        else:
+            cnnModel.fTrain(X_train=data.X_train,
+                            y_train=Y_train,
+                            Y_segMasks_train=data.Y_segMasks_train,
+                            X_valid=data.X_validation,
+                            y_valid=Y_validation,
+                            Y_segMasks_valid=data.Y_segMasks_validation,
+                            X_test=data.X_test,
+                            y_test=Y_test,
+                            Y_segMasks_test=data.Y_segMasks_test,
+                            sOutPath=data.outPutFolderDataPath,
+                            patchSize=[data.patchSizeX, data.patchSizeY, data.patchSizeZ],
+                            batchSizes=dlnetwork.batchSize,
+                            learningRates=dlnetwork.learningRate,
+                            iEpochs=dlnetwork.epochs,
+                            dlnetwork=dlnetwork)
+
+        print('==== Network training finished ====')
+
+    else:  # prediction
+        predictions = cnnModel.fPredict(X_test=data.X_test,
+                          Y_test=Y_test,
+                          Y_segMasks_test=data.Y_segMasks_test,
+                          sModelPath=savedmodel.savemodel,
+                          batch_size=dlnetwork.batchSize,
+                          usingClassification=dlnetwork.usingClassification,
+                          usingSegmentationMasks=data.usingSegmentationMasks,
+                          dlnetwork=dlnetwork)
+
+        if data.usingSegmentationMasks:
+            data.handlepredictionssegmentation(predictions)
+        else:
+            # TODO
+        print('==== Network testing finished ====')
 
 
 if __name__ == "__main__": # for command line call
@@ -180,26 +219,12 @@ if __name__ == "__main__": # for command line call
     # parse input
     cfg = fParseConfig(args.config[0])
 
-    lTrain = cfg['lTrain']  # training or prediction
-
-    sdata = Data(cfg)
-    # parse database
-    sdata.getAllDicomsPathList()
+    data = Data(cfg)
     # patch and split into training, val, test set
-    sdata.generateDataset()
+    data.generateDataset()
 
+    # get network parameters
+    dlnetwork = Dlnetwork(cfg)
 
-    if lTrain:
-        ########################
-        ## artifact detection ##
-        ## ---- training ---- ##
-        ########################
-        fTrainArtDetection()
-
-
-    else:
-        ########################
-        ## artifact detection ##
-        ## --- prediction --- ##
-        ########################
-        fPredictArtDetection()
+    print('==== Artifact detection ====')
+    fArtDetection(data, dlnetwork, cfg['sMode'])
