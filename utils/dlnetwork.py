a0fd00c9016f44c2a6b156474474095a63071318
Thomas Kuestner <thomas.kuestner@kcl.ac.uk>
2019-06-21 12:02:58 +0100

    add support for data augmentation

--- utils/dlnetwork.py
+++ utils/dlnetwork.py
@@ -21,3 +21,33 @@ class Dlnetwork:
         self.weightdecay = cfg['optimizer']['weightdecay']
         # SGD
         self.nesterov = cfg['optimizer']['nesterov']
+
+        # data augmentation
+        self.dataAugm_featurewise_center = cfg['dataAugmentation']['featurewise_center']
+        self.dataAugm_samplewise_center = cfg['dataAugmentation']['samplewise_center']
+        self.dataAugm_featurewise_std_normalization = cfg['dataAugmentation']['featurewise_std_normalization']
+        self.dataAugm_samplewise_std_normalization = cfg['dataAugmentation']['samplewise_std_normalization']
+        self.dataAugm_zca_whitening = cfg['dataAugmentation']['zca_whitening']
+        self.dataAugm_zca_epsilon = cfg['dataAugmentation']['zca_epsilon']
+        self.dataAugm_rotation_range = cfg['dataAugmentation']['rotation_range']
+        self.dataAugm_width_shift_range = cfg['dataAugmentation']['width_shift_range']
+        self.dataAugm_height_shift_range = cfg['dataAugmentation']['height_shift_range']
+        self.dataAugm_shear_range = cfg['dataAugmentation']['shear_range']
+        self.dataAugm_zoom_range = cfg['dataAugmentation']['zoom_range']
+        self.dataAugm_channel_shift_range = cfg['dataAugmentation']['channel_shift_range']
+        self.dataAugm_fill_mode = cfg['dataAugmentation']['fill_mode']
+        self.dataAugm_cval = cfg['dataAugmentation']['cval']
+        self.dataAugm_horizontal_flip = cfg['dataAugmentation']['horizontal_flip']
+        self.dataAugm_vertical_flip = cfg['dataAugmentation']['vertical_flip']
+        self.dataAugm_rescale = cfg['dataAugmentation']['rescale']
+        self.dataAugm_histogram_equalization = cfg['dataAugmentation']['histogram_equalization']
+        self.dataAugm_contrast_stretching = cfg['dataAugmentation']['contrast_stretching']
+        self.dataAugm_adaptive_equalization = cfg['dataAugmentation']['adaptive_equalization']
+
+        if (self.dataAugm_featurewise_center) | (self.dataAugm_samplewise_center) | (self.dataAugm_featurewise_std_normalization) | \
+            (self.dataAugm_samplewise_std_normalization) | (self.dataAugm_zca_whitening) | (self.dataAugm_horizontal_flip) | \
+            (self.dataAugm_vertical_flip) | (self.dataAugm_rescale) | (self.dataAugm_histogram_equalization) | \
+            (self.dataAugm_contrast_stretching) | (self.dataAugm_adaptive_equalization):
+            self.dataAugmentation = True
+        else:
+            self.dataAugmentation = False
