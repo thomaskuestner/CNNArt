58bba085444ed2081e401c4d1374fc9a2b04d9d3
Thomas Kuestner <thomas.kuestner@kcl.ac.uk>
2019-08-08 10:25:37 +0100

    add TF patching method

--- utils/RigidPatching.py
+++ utils/RigidPatching.py
@@ -4,6 +4,7 @@ Copyright: 2016-2019 Thomas Kuestner (thomas.kuestner@med.uni-tuebingen.de) unde
 '''
 
 import numpy as np
+import tensorflow as tf
 import math
 
 from utils.Label import Label
@@ -516,6 +517,48 @@ def fRigidPatching3D_maskLabeling(dicom_numpy_array, patchSize, patchOverlap, ma
         print("Rigid patching done for %s " % dataset.getPathdata())
         #print(dLabels)
         return dPatches, dLabels#, nbPatches
+
+def fRigidPatching3D_maskLabeling_tf(dicom_numpy_array, patchSize, patchOverlap, mask_numpy_array, ratio_labeling, dataset=None, dopatching=True):
+    #ToDo odd patch size not supported!
+
+    dOverlap = tf.math.round(tf.math.multiply(patchSize, patchOverlap))
+    dNotOverlap = tf.math.round(tf.math.multiply(patchSize, (1 - patchOverlap)))
+
+    size_zero_pad = tf.Variable(([tf.math.ceil((tf.shape(dicom_numpy_array)[0] - dOverlap[0]) / (dNotOverlap[0])) * dNotOverlap[0] + dOverlap[0],
+                               tf.math.ceil((tf.shape(dicom_numpy_array)[1] - dOverlap[1]) / (dNotOverlap[1])) * dNotOverlap[1] + dOverlap[1],
+                               tf.math.ceil((tf.shape(dicom_numpy_array)[2] - dOverlap[2]) / (dNotOverlap[2])) * dNotOverlap[2] + dOverlap[2]]))
+    zero_pad = tf.Variable(([int(size_zero_pad[0]) - dicom_numpy_array.shape[0],
+                          int(size_zero_pad[1]) - dicom_numpy_array.shape[1],
+                          int(size_zero_pad[2]) - dicom_numpy_array.shape[2]]))
+    zero_pad_part = tf.Variable(([int(math.ceil(zero_pad[0] / 2)),
+                               int(math.ceil(zero_pad[1] / 2)),
+                               int(math.ceil(zero_pad[2] / 2))]))
+
+    Img_zero_pad = tf.pad(dicom_numpy_array,
+                          ((zero_pad_part[0], zero_pad[0] - zero_pad_part[0]),
+                           (zero_pad_part[1], zero_pad[1] - zero_pad_part[1]),
+                           (zero_pad_part[2], zero_pad[2] - zero_pad_part[2])),
+                          mode='constant')
+
+    #nbPatches = ((size_zero_pad[0]-patchSize[0])/((1-patchOverlap)*patchSize[0])+1)*((size_zero_pad[1]-patchSize[1])/((1-patchOverlap)*patchSize[1])+1)*((size_zero_pad[2]-patchSize[2])/(tf.math.round((1-patchOverlap)*patchSize[2]))+1)
+
+    nbPatches_in_Y = int((size_zero_pad[0] - dOverlap[0]) / dNotOverlap[0])
+    nbPatches_in_X = int((size_zero_pad[1] - dOverlap[1]) / dNotOverlap[1])
+    nbPatches_in_Z = int((size_zero_pad[2] - dOverlap[2]) / dNotOverlap[2])
+    nbPatches = nbPatches_in_X * nbPatches_in_Y * nbPatches_in_Z
+
+    dPatches = tf.zeros((patchSize[0], patchSize[1], patchSize[2], int(nbPatches)), dtype=float)
+    idxPatch = 0
+
+    for iZ in range(0, int(size_zero_pad[2] - dOverlap[2]), int(dNotOverlap[2])):
+        for iY in range(0, int(size_zero_pad[0] - dOverlap[0]), int(dNotOverlap[0])):
+            for iX in range(0, int(size_zero_pad[1] - dOverlap[1]), int(dNotOverlap[1])):
+                dPatch = Img_zero_pad[iY:iY + patchSize[0], iX:iX + patchSize[1], iZ:iZ + patchSize[2]]
+                dPatches[:, :, :, idxPatch] = dPatch
+                idxPatch += 1
+
+    return dPatches
+
 
 def fcalculatepatches(imageSize, patchSize, patchOverlap):
 
